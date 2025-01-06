4083949078c0415bf270208c87f906e25a305e82
Sreevats Ramesh Ravillu <55175550+sreevatsravillu@users.noreply.github.com>
2025-01-06 23:52:16 +0530

    added majors

--- routes/formBuilder.js
+++ routes/formBuilder.js
@@ -8,20 +8,20 @@ router.get('/', (req, res) => {
 });
 
 router.post('/create', (req, res) => {
-  console.log('FB.js 11',req.body)
-  const { groupSize, skillName } = req.body;
-  console.log('FB.js 13', skillName, groupSize )
+  console.log(req.body)
+  const { formName, groupSize, skillName } = req.body;
+  console.log( formName, skillName, groupSize )
   const forms = readJSONFile('forms.json');
   const newForm = {
     skills: skillName.map(name => ({ skillName: name, required: true })),
     groupSize: parseInt(groupSize, 10),
     genders:['Male','Female','Prefer not to say'],
     pursuingYr:["First Year","Second Year","Third Year","Fourth Year","Fifth Year"],
-    majors:['MKTG','FIN','Explore'],
+    majors:['MKTG','FIN','Explore', 'Entrepreneurship', 'Accounting','MIS','Supply Chain','Psycology','MGMT'],
    ethnicity:['African American or Black','Asian - East Asian (e.g., Chinese, Japanese, Korean)',
     'Asian - South Asian (e.g., Indian, Pakistani, Bangladeshi)','Asian - Southeast Asian (e.g., Vietnamese, Filipino, Thai)',
     'Hispanic or Latino/a/x','Middle Eastern or North African (MENA)',
-   ' Native American or Alaska Native',
+    'Native American or Alaska Native',
     'Native Hawaiian or Other Pacific Islander',
   'White',
   'Multiracial'
@@ -37,9 +37,9 @@ router.post('/create', (req, res) => {
    "N.U.in Scotland – University of Glasgow",
     "N.U.in Spain – Saint Louis University Madrid"]
   };
-  forms.push(newForm);
+  forms[formName] = newForm;
   writeJSONFile('forms.json', forms);
-  res.redirect('/dashboard');
+  res.redirect('/');
 });
 
 module.exports = router;
